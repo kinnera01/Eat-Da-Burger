9af873cc0d7f11d4f20e1efc87531fda9575b811
kinnera01 <kinnera.bezawada1@gmail.com>
2017-11-03 09:45:08 -0400

    added footer

--- public/assets/js/sounds.js
+++ public/assets/js/sounds.js
@@ -6,11 +6,11 @@ var audio0 = new Audio('https://soundbible.com/mp3/TomCat-Mr_Smith-2055290520.mp
 var audio1 = new Audio('https://soundbible.com/mp3/Sheep Bleating-SoundBible.com-1373580685.mp3');
 var audio2 = new Audio('https://soundbible.com/mp3/Turkey Gobble-SoundBible.com-312252853.mp3');
 var audio3 = new Audio('https://soundbible.com/mp3/Dinosaur Roar-SoundBible.com-605392672.mp3');
-var audio4 = new Audio('https://soundbible.com/mp3/Lion-SoundBible.com-621763115.mp3');
+
 
 
 // Array of sounds
-var soundArray = [audio0, audio1, audio2, audio3, audio4];
+var soundArray = [audio0, audio1, audio2, audio3];
 
 
 // Click Listener for burger button
