4c739f3020bde9f458ba8979ecc25fb6c59882d2
kinnera01 <kinnera.bezawada1@gmail.com>
2017-11-02 20:04:13 -0400

    connections

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require('method-override');
 var app = express();
-var port = process.env.port || 3030;
+var port = process.env.PORT || 3030;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,24 +15,6 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// connection to sql db
-var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Kinnera06",
-  database: "burgers_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-
-  console.log("connected as id " + connection.threadId);
-});
 var routes = require('./controllers/controller.js');
 app.use('/', routes);
 
